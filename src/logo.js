11487273c2326288367bea5ef332abcd03bec482
Sanoj Kumar <sanojsanoj638@gmail.com>
2022-06-05 18:54:45 +0530

    done

---
+++ src/logo.js
@@ -0,0 +1,9 @@
+function image (){
+    let logo = document.createElement('img');
+    logo.setAttribute('src','https://www.pngitem.com/pimgs/m/279-2799340_blog-red-icon-note-taking-icon-hd-png.png');
+    logo.setAttribute('id','logoImg');
+    return logo;
+}
+
+export default image;
+
